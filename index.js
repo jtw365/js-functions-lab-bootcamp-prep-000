c6fca9d91781201d911db1e78be1a8cd8c1077ef
Jonathan Williams <jtw365@live.missouristate.edu>
2018-02-01 04:05:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 // write your code below!
-function happyHolidays() {
-  console.log('Happy holidays!')
-}
-
-function happyHolidaysTo(name) {
-  console.log(`Happy holidays, ${name}`)
-}
-
-function happyHolidayTo(holiday, name) {
-  console.log(`Happy ${holiday}, ${name}!`)
-}
-
-function holidayCountdown(days, holiday) {
-  console.log(`It's ${days} days until ${holiday}!`)
+function functions(happyHolidays) {
+  return happyHolidays
 }
